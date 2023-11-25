89d79669b5bdc8c6993ccf396e209bd088de22e2
markjamesgit <142849183+markjamesgit@users.noreply.github.com>
2023-11-26 02:29:52 +0800

    Added Footer Content

--- php/landing-page.php
+++ php/landing-page.php
@@ -52,13 +52,9 @@
         </div>
         <div class="products-container">
                 <img src="../assets/imgs/ball.jpg"class="product" alt="">
-
                 <img src="../assets/imgs/bag.jpg" class="product" alt="">
-
                 <img src="../assets/imgs/shoes.jpg" class="product" alt="">
-
                 <img src="../assets/imgs/short.jpg" class="product" alt="">
-
                 <img src="..\assets\imgs\sweater.jpg" class="product" alt="">
         </div>
     </div>
@@ -71,9 +67,7 @@
         </div>
         <div class="event-container">
             <img src="../assets/imgs/sale1.jpg" class="event-promo" alt="">
-
             <img src="../assets/imgs/sale3.jpg" class="event-promo" alt="">
-
             <img src="..\assets\imgs\sale2.jpg" class="event-promo" alt="">
         </div>
     </div>
@@ -134,5 +128,42 @@
             <i class="fa-brands fa-square-instagram" style="color: #000000;"></i>
         </div>
     </div>
+    
+    <!--Footer-->
+    <footer>
+        <div class="footer-container">
+            <div class="information">
+                <span>ABOUT US</span>
+                <p>Welcome to Vulcan Sports Hub, where passion meets performance. 
+                    At Vulcan, we've crafted more than just a sports store; we've built a hub for enthusiasts, 
+                    athletes, and anyone embracing an active lifestyle.
+                </p>
+            </div>
+            <div class="information">
+                <span>LOCATION</span>
+                <p>213 New York St. Cubao, Metro, Manila Philippines</p>
+            </div>
+            <div class="information">
+                <span>TALK TO PROFESSIONALS</span>
+                <div class="professionals-gmail">
+                    <div class="gmails"><i class="fa-solid fa-envelope"></i> Clyde Reyniel C. Mondero</div>
+                    <div class="gmails"><i class="fa-solid fa-envelope"></i> Andrei John D. Poma</div>
+                    <div class="gmails"><i class="fa-solid fa-envelope"></i> Bhenz Mharl L. Bartolome</div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-navigation">
+            <div class="left-content">
+                <div><i class="fa-solid fa-location-dot"></i> Philippines</div>
+                <div class="gray-text"><i class="fa-solid fa-copyright"></i> 2023, Vulcan Sports Hub Inc. All Rights Reserved.</div>
+            </div>
+            <div class="right-content">
+                <p class="gray-text">Guide</p>
+                <p class="gray-text">Term of Use</p>
+                <p class="gray-text">Term of Sale</p>
+                <p class="gray-text">Vulcan Sports Hub Policy</p>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
