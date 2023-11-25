fa91354da19dfbde0e58e697f29532026b36be97
markjamesgit <142849183+markjamesgit@users.noreply.github.com>
2023-11-25 20:33:52 +0800

    Added Navbar

---
+++ php/landing-page.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../styles/landing-page.css">
+    <title>VULCAN</title>
+</head>
+<body>
+    <!--Header Contents-->
+    <header>
+        <div class="header-contents">
+            <div class="logo-and-title">
+                <img src="../assets/imgs/Vulcan Logo.png" alt="">
+                <span>VULCAN</span>
+            </div>
+            <div class="search-bar">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="search" placeholder="Search...">
+            </div>
+            <div class="add-to-cart">
+                <p class="cart-count">0</p>
+                <i class="fa-solid fa-cart-shopping"></i>
+            </div>
+            <div class="login-and-signup">
+                <a href="login.php"><p>Sign In</p></a>
+                <div class="line"></div>
+                <a href="login.php"><p>Sign Up</p></a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
