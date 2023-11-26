0820fdb6bcd2d365dcdbc23ca0acf11c1a73005a
markjamesgit <142849183+markjamesgit@users.noreply.github.com>
2023-11-26 12:11:21 +0800

    Added Dashboard

---
+++ php/dashboard.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/dashboard.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <title>DASHBOARD</title>
+</head>
+<body>
+    <div class="dashboard-content">
+        <div class="logo">
+            <img src="..\assets\imgs\Vulcan Logo.png" alt="">
+            <span>VULCAN</span>
+        </div>
+        <hr class="line">
+        <div class="admin-tools">
+            <div class="admin-tools-label">
+                <p>Admin Tools</p>
+            </div>
+            <div class="dashboard-features">
+                <i class="fa-brands fa-dashcube"></i>
+                <span class="">DASHBOARD</span>
+            </div>
+            <div class="dashboard-features">
+                <i class="fa-brands fa-product-hunt"></i>
+                <span class="">ADD ITEMS</span>
+            </div>
+            <div class="dashboard-features">
+                <i class="fa-solid fa-sitemap"></i>
+                <span class="">CATEGORY</span>
+            </div>
+            <div class="dashboard-features">
+                <i class="fa-solid fa-boxes-stacked"></i>
+                <span class="">INVENTORY</span>
+            </div>
+            <div class="dashboard-features">
+               <i class="fa-solid fa-list-check"></i>
+               <span class="">SALES</span>
+            </div>
+        </div>
+        <div class="sign-out">
+            <span>SIGN OUT</span>
+            <i class="fa-solid fa-right-from-bracket"></i>
+        </div>
+    </div>
+</body>
+</html>
