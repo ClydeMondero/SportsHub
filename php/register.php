4e97be776581790305894c9b4d54b096623f069a
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2023-11-26 13:36:39 +0800

    Fixed Logos in SignIn and SignUp Page

--- php/register.php
+++ php/register.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="../styles/register.css">
-  <link rel="shortcut icon" type="image/x-icon" href="../assets/imgs/logo.png">
+  <link rel="shortcut icon" type="image/x-icon" href="../assets/imgs/Vulcan Logo.png">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <title>VULCAN SPORT SIGN UP</title>
@@ -14,7 +14,7 @@
 
 <body>
   <div class="form-container">
-    <h1><img src="../assets/imgs/logo.png" alt="" height="50" width="50">SIGN UP</h1>
+    <h1><img src="../assets/imgs/Vulcan Logo.png" alt="" height="50" width="50"> SIGN UP</h1>
     <form method="POST" class="form">
       <div class="form-input">
         <label>Full Name:</label>
