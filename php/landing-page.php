e47d51d6e9b7eabc48204b091ca1c834d83ff5d5
Clyde Mondero <clydereinyelmondero0324@gmail.com>
2023-11-26 14:51:13 +0800

    Made Header a PHP File and added Shopping Page

--- php/landing-page.php
+++ php/landing-page.php
@@ -10,66 +10,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="image/x-icon" href="../assets/imgs/Vulcan Logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../styles/landing-page.css">    
     <title>VULCAN</title>
 </head>
 <body>
-    <!--Header-->
-    <div class="header">
-        <div class="logo-and-title">
-            <img src="../assets/imgs/Vulcan Logo.png" alt="">
-            <span>VULCAN</span>
-        </div>
-
-        <div class="center">
-            <div class="search-bar">
-                <i class="fa-solid fa-magnifying-glass"></i>
-                <input type="search" placeholder="Search...">
-            </div>
-
-            <div class="add-to-cart">
-                <p class="cart-count">0</p>
-                <i class="fa-solid fa-cart-shopping"></i>
-            </div>
-        </div>
-       
-        <div class="login-and-signup">
-            <a href="login.php"><p>Sign In</p></a>
-            <div class="line"></div>
-            <a href="register.php"><p>Sign Up</p></a>
-        </div>
-
-        <div class="profile-and-logout">
-            <a href="login.php"><p>Profile</p></a>
-            <div class="line"></div>
-            <a href="logout.php"><p>Logout</p></a>
-        </div>
-
-        <?php
-            if($loggedIn){
-                echo '<style>
-                    .login-and-signup{display: none !important;}
-                </style>';
-
-                echo '<style>
-                    .profile-and-logout{display: flex !important;}
-                </style>';
-            } else{
-                echo '<style>
-                    .login-and-signup{display: flex !important;}
-                </style>';
-
-                echo '<style>
-                    .profile-and-logout{display: none !important;}
-                </style>';
-            }
-        ?>
-
-    </div>    
+    <?php include("header.php") ?>         
     
         <!-- Latest -->
-        <div class="trending content">
+     <div class="trending content">
         <div class="trending-title">
             <span>Latest</span>            
         </div>
@@ -80,7 +30,7 @@
             <p class="trending-caption">Speed Beyond Your Wildest Dreams</p>
             <p class="description">Make it real with Mercurial Dream Speed 7.
             </p>
-            <button>Shop</button>
+            <a href="shopping-page.php"><button>Shop Now</button></a>
         </div>
     </div>
 
@@ -125,7 +75,7 @@
             <p class="description">Made from at least 20% recycled content by weight, 
                 it has a supportive fit and stable ride, with a lightweight feel that easily transitions from your workout to hangout.
             </p>
-            <button type="submit">Shop</button>
+            <a href="shopping-page.php"><button>Shop Now</button></a>
         </div>
     </div>
 
---
+++ php/shopping-page.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    $loggedIn = isset($_SESSION['loggedin']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/shopping-page.css">
+    <link rel="stylesheet" type="image/x-icon" href="../assets/imgs/Vulcan Logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <title>Shopping Page</title>
+</head>
+<body>
+    <?php include("header.php") ?>
+</body>
+</html>
