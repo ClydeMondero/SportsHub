527310b4704ca27a7e185a14d4689f2385d62246
markjamesgit <142849183+markjamesgit@users.noreply.github.com>
2023-11-27 16:47:35 +0800

    Update Sign In and Sign Out

--- php/landing-page.php
+++ php/landing-page.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="image/x-icon" href="../assets/imgs/Vulcan Logo.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/imgs/Vulcan Logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../styles/landing-page.css">    
     <title>VULCAN</title>
@@ -115,7 +115,6 @@
             <i class="fa-brands fa-square-instagram" style="color: #000000;"></i>
         </div>
     </div>
-    
-    <?php include "footer.php" ?>    
+    <?php include ("footer.php"); ?>    
 </body>
 </html>
--- php/login.php
+++ php/login.php
@@ -6,15 +6,22 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="../styles/login.css">
-  <link rel="stylesheet" type="image/x-icon" href="../assets/imgs/Vulcan Logo.png">
+  <link rel="shortcut icon" type="image/x-icon" href="../assets/imgs/Vulcan Logo.png">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <title>VULCAN SPORT SIGN IN</title>
 </head>
 
-<body>
+<body>       
+  
+  <div class="back">
+    <a href="landing-page.php"><i class="fa-solid fa-chevron-left"><span> Back</span></i></a>
+  </div>
   <div class="form-container">
-    <h1><img src="../assets/imgs/Vulcan Logo.png" alt="" height="50" width="50"> SIGN IN</h1>
+    <div class ="row-title-and-exit">
+    <img src="../assets/imgs/Vulcan Logo.png" alt="" height="100px" width="100px">
+        <h1>SIGN IN</h1>
+    </div>
     <form method="POST" class="form">
 
       <div class="form-input">
@@ -27,11 +34,11 @@
         <input type="password" name="password" placeholder="***************" required min="8" max="16" />
       </div>
       <input type="submit" class="login-btn" name="btnLogin" value="SIGN IN" />
-      <span>Forgot password?</span>
+      <p>Forgot password?</p>
       <p>Don't have an account? <a href="register.php">Sign Up now</a></p>
     </form>
   </div>
-
+  
   <?php
     include("conn.php");
     
