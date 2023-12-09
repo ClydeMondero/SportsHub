ee05c36c811ba978180d7348c5d033b5a816edb5
markjamesgit <142849183+markjamesgit@users.noreply.github.com>
2023-12-09 15:17:53 +0800

    Update Profile

--- php/profile.php
+++ php/profile.php
@@ -41,6 +41,7 @@
                 <th>Order Date</th>
                 <th>Order Arrival Date</th>
                 <th>Order Status</th>
+                <th>Actions</th>
             </tr>
             <tr>
                 <td>YOYO</td>
@@ -50,6 +51,7 @@
                 <td>06-12-2023</td>
                 <td>12-11-2023</td>
                 <td>Delivered</td>
+                <td></td>
             </tr>
             <tr>
                 <td>YOYO</td>
@@ -59,6 +61,7 @@
                 <td>06-12-2023</td>
                 <td>12-11-2023</td>
                 <td>Delivered</td>
+                <td></td>
             </tr>
         </table>
     </div>
