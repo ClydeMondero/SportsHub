bb5de5151fb684a92ad4091175836f9d5b8be861
rijin0408 <bhenzmharlbartolome@gmail.com>
2023-12-09 13:04:00 +0800

    Update Search Function in products, sales,account and back button in update seller

--- php/sales.php
+++ php/sales.php
@@ -1,3 +1,44 @@
+<?php
+    include('conn.php');
+    session_start();
+
+    $search = isset($_GET['search']) ? $_GET['search'] : '';
+    
+    $selectQuery = 'SELECT
+        tborders.order_id,
+        tborders.product_id,
+        tbproducts.product_name,
+        tborders.user_id,
+        tbusers.user_username,
+        tborders.order_quantity,
+        tborders.order_price,
+        tborders.order_payment_method,
+        tborders.order_address,
+        tborders.order_date,
+        tborders.order_arrival_date,
+        tborders.order_status
+    FROM
+        tborders
+    INNER JOIN
+        tbproducts ON tborders.product_id = tbproducts.product_id
+    INNER JOIN
+        tbusers ON tborders.user_id = tbusers.user_id
+    WHERE
+        tborders.order_id LIKE "%' . $search . '%"
+        OR tbproducts.product_name LIKE "%' . $search . '%"
+        OR tbusers.user_username LIKE "%' . $search . '%"
+        OR tborders.order_quantity LIKE "%' . $search . '%"
+        OR tborders.order_price LIKE "%' . $search . '%"
+        OR tborders.order_payment_method LIKE "%' . $search . '%"
+        OR tborders.order_address LIKE "%' . $search . '%"
+        OR tborders.order_date LIKE "%' . $search . '%"
+        OR tborders.order_arrival_date LIKE "%' . $search . '%"
+        OR tborders.order_status LIKE "%' . $search . '%"';
+        
+    $result = $conn->query($selectQuery);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,9 +57,10 @@
              <div class="table-actions">
                     <div class="product-search">  
                         <select name="filter" id="filter">
-                            <option value="returned">Returned</option>
-                            <option value="returned">Delivered</option>
-                            <option value="returned">In Transit</option>
+                            <option value="Returned">Returned</option>
+                            <option value="Delivered">Delivered</option>
+                            <option value="Pending">Pending</option>
+                            <option value="Cancelled"></option>
                         </select>                      
                         <i class="fa-solid fa-magnifying-glass"></i>
                         <form method="GET" action="" id="searchForm">
@@ -31,13 +73,36 @@
                     <table>
                         <tr>
                             <th>Order ID</th>
+                            <th>Product ID</th>
                             <th>Product Name</th>
+                            <th>User ID</th>
                             <th>Username</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
                             <th>Payment Method</th>
+                            <th>Address</th>
                             <th>Order Date</th>
                             <th>Order Arrived Date</th>
                             <th>Order Status</th>
                         </tr>
+                        <?php
+                            while ($row = $result->fetch_assoc()) {
+                                echo '<tr>';
+                                echo '<td>' . $row['order_id'] . '</td>';
+                                echo '<td>' . $row['product_id'] . '</td>';
+                                echo '<td>' . $row['product_name'] . '</td>';
+                                echo '<td>' . $row['user_id'] . '</td>';
+                                echo '<td>' . $row['user_username'] . '</td>';
+                                echo '<td>' . $row['order_quantity'] . '</td>';
+                                echo '<td>' . $row['order_price'] . '</td>';
+                                echo '<td>' . $row['order_payment_method'] . '</td>';
+                                echo '<td>' . $row['order_address'] . '</td>';
+                                echo '<td>' . $row['order_date'] . '</td>';
+                                echo '<td>' . $row['order_arrival_date'] . '</td>';
+                                echo '<td>' . $row['order_status'] . '</td>';
+                                echo '</tr>';
+                            }
+                        ?>
                     </table>
                 </div>
             </div>
